b8d5e6daf04ca010d014586b8592d45837d072fd
IchwanArdi <ichwanpwt22@gmail.com>
2025-02-23 07:35:21 +0700

    first commit

---
+++ api/project.js
@@ -0,0 +1,7 @@
+// program untuk kategori filter
+document.querySelectorAll('.nav-link').forEach((link) => {
+  link.addEventListener('click', function () {
+    document.querySelector('.nav-link.active')?.classList.remove('active');
+    this.classList.add('active');
+  });
+});
